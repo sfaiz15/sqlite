774e8aa80e64895c9629491acb4bab3d79e723d6
sfaiz15 <107978726+sfaiz15@users.noreply.github.com>
2022-07-28 00:18:16 -0400

    commit

---
+++ sql.sql
@@ -0,0 +1,14 @@
+--CREATE TABLE USERS (username TEXT NOT NULL PRIMARY KEY,
+--password NOT NULL, email NOT NULL);
+
+ --INSERT INTO users (username, password, email)
+ --VALUES ("joe", "joespassword", "joey@student.gsu.edu");
+
+--DELETE FROM USERS
+--WHERE username ="mike"; 
+
+--SELECT email FROM users
+--WHERE username LIKE "joe";  
+
+SELECT * FROM users;
+
